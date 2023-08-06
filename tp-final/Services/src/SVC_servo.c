c4e6dac5e4d60a543689b2479358fa0be5407674
Leandro Soria <leandrosoria@hotmail.com>
2023-08-06 14:39:55 -0300

    SVC_servo.c cleanup

--- tp-final/Services/src/SVC_servo.c
+++ tp-final/Services/src/SVC_servo.c
@@ -1,10 +1,7 @@
+#include <assert.h>
 #include "API_delay.h"
 #include "API_pwm.h"
 #include "SVC_servo.h"
-
-#define SERVO_IDLE_DC 6.5f
-#define SERVO_0_DEGREES_DC 2.5f
-#define SERVO_180_DEGREES_DC 11.0f
 
 #define SERVO_FSM_DELAY_TIMEOUT_MS 2000
 
@@ -13,18 +10,19 @@ typedef enum
     SERVO_IDLE = 0,
     SERVO_POSITION_0,
     SERVO_POSITION_180,
+    SERVO_FSM_STATES_QTY, ///< Always keep this value at the bottom!
 } ServoFSMState;
 
+static const float SERVO_DUTY_CYCLES[SERVO_FSM_STATES_QTY] =
+{
+    [SERVO_IDLE] = 6.5f,
+    [SERVO_POSITION_0] = 2.5f,
+    [SERVO_POSITION_180] = 11.0f,
+};
 
 static delay_t servo_delay = {};
 static ServoFSMState current_state = SERVO_IDLE;
 static bool_t button_pressed = false;
-
-
-void svc_servo_change_state()
-{
-    button_pressed = true;
-}
 
 void svc_servo_init()
 {
@@ -39,14 +37,12 @@ void svc_servo_fsm_update()
     switch(current_state)
     {
     case SERVO_IDLE:
-        pwm_set_dc(SERVO_IDLE_DC);
         if(button_pressed) {
             current_state = SERVO_POSITION_0;
             button_pressed = false;
         }
         break;
     case SERVO_POSITION_0:
-        pwm_set_dc(SERVO_0_DEGREES_DC);
         if(button_pressed) {
             current_state = SERVO_IDLE;
             button_pressed = false;
@@ -56,7 +52,6 @@ void svc_servo_fsm_update()
         }
         break;
     case SERVO_POSITION_180:
-        pwm_set_dc(SERVO_180_DEGREES_DC);
         if(button_pressed) {
             current_state = SERVO_IDLE;
             button_pressed = false;
@@ -65,6 +60,15 @@ void svc_servo_fsm_update()
             current_state = SERVO_POSITION_0;
         }
         break;
-
+    default:
+        assert(false && "Invalid state");
+        break;
     }
+
+    pwm_set_dc(SERVO_DUTY_CYCLES[current_state]);
+}
+
+void svc_servo_change_state()
+{
+    button_pressed = true;
 }
