70d8b103f56602a5d71273758f317d6ee2745c3e
Leandro Soria <leandrosoria@hotmail.com>
2023-08-09 15:30:43 -0300

    Implementation of read operations in i2c_port files and also added doxy comments (to be completed later)

--- tp-final/Drivers/MPU6500/inc/i2c_port.h
+++ tp-final/Drivers/MPU6500/inc/i2c_port.h
@@ -1,14 +1,52 @@
 #pragma once
+#include <stdint.h>
 
 void i2c_port_init(uint8_t slave_address);
 
+/// @brief Read a single bit from a register.
+/// @param register_address 
+/// @param bit_number 
+/// @param data 
+void i2c_port_read_bit(uint8_t register_address, uint8_t bit_number, uint8_t* data);
 
-void i2c_port_write_bit(uint8_t register_address, uint8_t bit_number, uint8_t data);
-/*! Write a range of bits to a register */
-void i2c_port_write_bits(uint8_t register_address, uint8_t bit_start, uint8_t length, uint8_t data);
-/*! Write a value to a register */
-void i2c_port_write_byte(uint8_t register_address, uint8_t data);
-/*! Write a sequence to data to a sequence of registers */
-void i2c_port_write_bytes(uint8_t register_address, uint8_t length, uint8_t* data);
+/// @brief Read a range of bits from a single register.
+/// @param register_address 
+/// @param bit_start 
+/// @param length 
+/// @param data 
+void i2c_port_read_bits(uint8_t register_address, uint8_t bit_start, uint8_t length, uint8_t* data);
 
+/// @brief Read a single register.
+/// @param register_address 
+/// @param data 
 void i2c_port_read_byte(uint8_t register_address, uint8_t* data);
+
+/// @brief Read data from consecutive registers.
+/// @param register_address 
+/// @param length 
+/// @param data 
+void i2c_port_read_bytes(uint8_t register_address, uint8_t length, uint8_t* data);
+
+/// @brief Write a single bit from a register
+/// @param register_address 
+/// @param bit_number 
+/// @param data 
+void i2c_port_write_bit(uint8_t register_address, uint8_t bit_number, uint8_t data);
+
+/// @brief Write a range of bits to a register
+/// @param register_address 
+/// @param bit_start 
+/// @param length 
+/// @param data 
+void i2c_port_write_bits(uint8_t register_address, uint8_t bit_start, uint8_t length, uint8_t data);
+
+/// @brief Write a value to a single register.
+/// @param register_address 
+/// @param data 
+void i2c_port_write_byte(uint8_t register_address, uint8_t data);
+
+/// @brief Write a data sequence to consecutive registers.
+/// @param register_address 
+/// @param length 
+/// @param data 
+void i2c_port_write_bytes(uint8_t register_address, uint8_t length, uint8_t* data);
--- tp-final/Drivers/MPU6500/src/i2c_port.c
+++ tp-final/Drivers/MPU6500/src/i2c_port.c
@@ -9,17 +9,44 @@ void i2c_port_init(uint8_t slave_address)
 {
     MX_I2C1_Init(&i2c_handler);
     port_slave_address = slave_address;
-
-    uint8_t who_am_i = 0;
-//    i2c_port_read_byte(0x75, &who_am_i);
-
-//    HAL_I2C_Mem_Read(&i2c_handler, MPU_6500_DEFAULT_ADDRESS | 0x01 , MPU_6500_REG_WHO_AM_I, 1, &who_am_i, sizeof(who_am_i), HAL_MAX_DELAY);
-    i2c_port_write_bits(0, 4, 3, 0b110);
 }
 
+void i2c_port_read_bit(uint8_t register_address, uint8_t bit_number, uint8_t* data)
+{
+    if(bit_number < 0 || bit_number > 7) { return; }
+
+    uint8_t register_value = 0x00;
+    HAL_I2C_Mem_Read(&i2c_handler, port_slave_address, register_address, 1, &register_value, 1, HAL_MAX_DELAY);
+
+    *data = (register_value >> bit_number & 0x01);
+}
+
+void i2c_port_read_bits(uint8_t register_address, uint8_t bit_start, uint8_t length, uint8_t* data)
+{
+    if (bit_start < 0 || bit_start > 7 || length < 1 || length > 8) { return; }
+
+    uint8_t register_value = 0x00;
+    HAL_I2C_Mem_Read(&i2c_handler, port_slave_address, register_address, 1, &register_value, 1, HAL_MAX_DELAY);
+
+    uint8_t bit_position = bit_start - length + 1;  // Calculate LSB position
+    uint8_t mask = (1 << length) - 1;
+    *data = (register_value >> bit_position) & mask;
+}
+
+void i2c_port_read_byte(uint8_t register_address, uint8_t* data)
+{
+    HAL_I2C_Mem_Read(&i2c_handler, port_slave_address, register_address, 1, data, 1, HAL_MAX_DELAY);
+}
+
+void i2c_port_read_bytes(uint8_t register_address, uint8_t length, uint8_t* data)
+{
+    HAL_I2C_Mem_Read(&i2c_handler, port_slave_address, register_address, 1, data, length, HAL_MAX_DELAY);
+}
 
 void i2c_port_write_bit(uint8_t register_address, uint8_t bit_number, uint8_t data)
 {
+    if(bit_number < 0 || bit_number > 7) { return; }
+
     uint8_t register_value = 0x00;
 
     // First retrieve existing value
@@ -30,9 +57,11 @@ void i2c_port_write_bit(uint8_t register_address, uint8_t bit_number, uint8_t da
 
     HAL_I2C_Mem_Write(&i2c_handler, port_slave_address, register_address, 1, &register_value, 1, HAL_MAX_DELAY);
 }
-///*! Write a range of bits to a register */
+
 void i2c_port_write_bits(uint8_t register_address, uint8_t bit_start, uint8_t length, uint8_t data)
 {
+    if (bit_start < 0 || bit_start > 7 || length < 1 || length > 8) { return; }
+
     uint8_t register_value = 0x00;
 
     // First retrieve existing value
@@ -45,19 +74,13 @@ void i2c_port_write_bits(uint8_t register_address, uint8_t bit_start, uint8_t le
     HAL_I2C_Mem_Write(&i2c_handler, port_slave_address, register_address, 1, &register_value, 1, HAL_MAX_DELAY);
 
 }
-///*! Write a value to a register */
+
 void i2c_port_write_byte(uint8_t register_address, uint8_t data)
 {
     HAL_I2C_Mem_Write(&i2c_handler, port_slave_address, register_address, 1, &data, 1, HAL_MAX_DELAY);
 }
-///*! Write a sequence to data to a sequence of registers */
+
 void i2c_port_write_bytes(uint8_t register_address, uint8_t length, uint8_t* data)
 {
-    HAL_I2C_Mem_Write(&i2c_handler, port_slave_address, register_address, length, data, length, HAL_MAX_DELAY);
-}
-
-
-void i2c_port_read_byte(uint8_t register_address, uint8_t* data)
-{
-    HAL_I2C_Mem_Read(&i2c_handler, port_slave_address, register_address, 1, data, 1, HAL_MAX_DELAY);
+    HAL_I2C_Mem_Write(&i2c_handler, port_slave_address, register_address, 1, data, length, HAL_MAX_DELAY);
 }
