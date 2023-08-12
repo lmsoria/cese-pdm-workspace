7825958c054e995f1d9ca7b0ea345c446249aff7
Leandro Soria <leandrosoria@hotmail.com>
2023-08-12 15:37:50 -0300

    Document delay_port files

--- tp-final/Drivers/MPU6500/src/delay_port.c
+++ tp-final/Drivers/MPU6500/src/delay_port.c
@@ -2,4 +2,6 @@
 
 #include "delay_port.h"
 
+ // Since this will run on an initialization stage (and for the sake of simplicity),
+ // We'll use a simple, blocking delay.
 void delay_port_wait(const uint32_t msec) { HAL_Delay(msec); }
